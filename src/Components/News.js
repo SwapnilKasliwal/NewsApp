92799f4d0670b4c2ce24fdb68d73f641ff996ba5
Swapnil Kasliwal <swapnilkasliwal2000@gmail.com>
2022-10-16 00:00:12 +0530

    raw application with router and other features

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import PropTypes from 'prop-types'
+import Spinner from './Spinner'
+
 
 export default class News extends Component {
+
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.string
+    }
     
+    static defaultProps = {
+        country: "in",
+        pageSize: "20"
+    }
      constructor(){
     super()
     this.state = {
@@ -13,51 +25,50 @@ export default class News extends Component {
    }
 
    async componentDidMount(){
-    let data = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=77aab9820db94af781584929c663b1c8&page=1&pageSize=20")
+    this.setState({loading: true})
+    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=77aab9820db94af781584929c663b1c8&page=1&pageSize=${this.props.pageSize}`)
     let newsArticles = await data.json()
     // This is the way we used always set the state of the state variables here in react.though we could have directly written
     // simply like other languages too
-    this.setState({articles: newsArticles.articles, totalArticles : newsArticles.totalResults})
+    this.setState({articles: newsArticles.articles, totalArticles : newsArticles.totalResults, loading: false})
    }
 
    handleNextPage = async ()=>{
-    if(this.state.page +1 > Math.ceil(this.state.totalArticles/20))
-    {
-        console.log("I am inside if of next")
-    }
-    else{
-        console.log("I am inside else of next")
-        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=77aab9820db94af781584929c663b1c8&page=${this.state.page+1}&pageSize=20`)
+    this.setState({loading: true})
+    if(!(this.state.page +1 > Math.ceil(this.state.totalArticles/20))){
+        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=77aab9820db94af781584929c663b1c8&page=${this.state.page+1}&pageSize=${this.props.pageSize}`)
         let newsArticles = await data.json()
         // This is the way we used always set the state of the state variables here in react.though we could have directly written
         // simply like other languages too
-        this.setState({articles: newsArticles.articles, page : this.state.page +1})
+        this.setState({articles: newsArticles.articles, page : this.state.page +1, loading: false})
     }
    }
    handlePreviousPage = async()=>{
-    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=77aab9820db94af781584929c663b1c8&page=${this.state.page-1}&pageSize=20`)
-    let newsArticles = await data.json()
-    // This is the way we used always set the state of the state variables here in react.though we could have directly written
-    // simply like other languages too
-    this.setState({articles: newsArticles.articles, page: this.state.page -1})
+    this.setState({loading: true})
+        let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=77aab9820db94af781584929c663b1c8&page=${this.state.page-1}&pageSize=${this.props.pageSize}`)
+        let newsArticles = await data.json()
+        // This is the way we used always set the state of the state variables here in react.though we could have directly written
+        // simply like other languages too
+        this.setState({articles: newsArticles.articles, page: this.state.page -1, loading: false})
 
    }
     
   render() {
     return (
-        <div className='container my-5'>
-            <h3>NewsApp - Top Headlines for the day</h3>
+        <div className='container center'>
+            <h1 className='text-center' style={{margin: '40px'}}>NewsApp - Top Headlines for the day</h1>
             <div className="row my-3">
+                {this.state.loading && <Spinner/>}
                 {this.state.articles.map((element)=>{
                    return <div key={element.url} className="col-md-3 my-2 mx-3">
-                    <NewsItem title={element.title?element.title.slice(0,40):""} description={element.description?element.description.slice(0,80):""} imgsource={element.urlToImage?element.urlToImage:"https://cdn.theathletic.com/app/uploads/2022/10/12091755/GettyImages-1240077873-1.jpg"} newsURL={element.url}/>
+                    <NewsItem title={element.title?element.title.slice(0,40):""} description={element.description?element.description.slice(0,80):""} imgsource={element.urlToImage?element.urlToImage:"https://cdn.theathletic.com/app/uploads/2022/10/12091755/GettyImages-1240077873-1.jpg"} newsURL={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
                     </div>
                 })}   
                 
             </div>
             <div className="container d-flex justify-content-between">
             <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousPage}> &larr; Previous</button>
-            <button type="button" className="btn btn-dark" onClick={this.handleNextPage}>Next &rarr;</button>
+            <button disabled={this.state.page +1 > Math.ceil(this.state.totalArticles/20)} type="button" className="btn btn-dark" onClick={this.handleNextPage}>Next &rarr;</button>
             </div>
         </div>
     )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {title, description,imgsource, newsURL} = this.props
+    let {title, description,imgsource, newsURL, author, date, source} = this.props
     return (
-        <div className="card" style={{width: "18rem"}}>
-        <img src={imgsource?imgsource:"https://cdn.theathletic.com/app/uploads/2022/10/12091755/GettyImages-1240077873-1.jpg"} className="card-img-top" alt="..." style={{height:"200px",width:"286px"}}/>
+        <div className="card">
+        <img src={imgsource?imgsource:"https://cdn.theathletic.com/app/uploads/2022/10/12091755/GettyImages-1240077873-1.jpg"} className="card-img-top" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title">{`${title}...`}</h5>
+          <h5 className="card-title">{`${title}...`}<span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:'1', left:'85%'}}>
+         {`${source}`}
+          </span></h5>
           <p className="card-text">{`${description}...`}</p>
+          <p className="card-text"><small className="text-muted">By {author?author:"Anonymous"} on {new Date(date).toGMTString()}</small></p>
           <a target={{_blank:true}} href={newsURL} type = "button" className="btn btn-sm btn-dark">Read More</a>
         </div>
       </div>
