c08dd1bb73f147ff1cbeafe8e2e00eb5feb59d5f
Swapnil Kasliwal <swapnilkasliwal2000@gmail.com>
2022-10-20 20:54:06 +0530

    Added infinite scroll and hid APIKEY

--- src/App.js
+++ src/App.js
@@ -8,21 +8,22 @@ import {
   Routes,
   Route
 } from 'react-router-dom';
-
 export default class App extends Component {
+  apiKey = process.env.REACT_APP_API_KEY
   pageSize = "12"
   render() {
+    console.log(this.apiKey)
     return (
       <Router>
         <NavBar/>
       <Routes>
-        <Route exact path='/' element = {<News key= "general" pageSize={this.pageSize} country="in" category="general"/>}></Route>
-        <Route exact path='/business' element = {<News  key= "business" pageSize={this.pageSize} country="in" category="business"/>}></Route>
-        <Route exact path='/entertainment' element = {<News key= "entertainment" pageSize={this.pageSize} country="in" category="entertainment"/>}></Route>
-        <Route exact path='/health' element = {<News key= "health" pageSize={this.pageSize} country="in" category="health"/>}></Route>
-        <Route exact path='/science' element = {<News key= "science" pageSize={this.pageSize} country="in" category="science"/>}></Route>
-        <Route exact path='/sports' element = {<News key= "sports" pageSize={this.pageSize} country="in" category="sports"/>}></Route>
-        <Route exact path='/technology' element = {<News  key= "technology" pageSize={this.pageSize} country="in" category="technology"/>}></Route>
+        <Route exact path='/' element = {<News key= "general" pageSize={this.pageSize} country="in" category="general" apiKey={this.apiKey}/>}></Route>
+        <Route exact path='/business' element = {<News  key= "business" pageSize={this.pageSize} country="in" category="business" apiKey={this.apiKey}/>}></Route>
+        <Route exact path='/entertainment' element = {<News key= "entertainment" pageSize={this.pageSize} country="in" category="entertainment" apiKey={this.apiKey}/>}></Route>
+        <Route exact path='/health' element = {<News key= "health" pageSize={this.pageSize} country="in" category="health" apiKey={this.apiKey}/>}></Route>
+        <Route exact path='/science' element = {<News key= "science" pageSize={this.pageSize} country="in" category="science" apiKey={this.apiKey}/>}></Route>
+        <Route exact path='/sports' element = {<News key= "sports" pageSize={this.pageSize} country="in" category="sports" apiKey={this.apiKey}/>}></Route>
+        <Route exact path='/technology' element = {<News  key= "technology" pageSize={this.pageSize} country="in" category="technology" apiKey={this.apiKey}/>}></Route>
       </Routes>
       </Router>
     )
--- src/Components/News.js
+++ src/Components/News.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import PropTypes from 'prop-types'
 import Spinner from './Spinner'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
 
     static propTypes = {
         country: PropTypes.string,
-        pageSize: PropTypes.string
+        pageSize: PropTypes.string,
+        apiKey: PropTypes.string
     }
     
     static defaultProps = {
@@ -18,40 +20,51 @@ export default class News extends Component {
     super(props)
     this.state = {
         articles : [],
-        loading : false,
-        page : 1
+        loading : true,
+        page : 1,
+        totalArticles : 0
     }
    }
    capitalize(element){
     return element.toLowerCase()[0].toUpperCase() + element.slice(1,)
    }
-   async updateNews(pageNo){
-    this.setState({loading: true})
-    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=77aab9820db94af781584929c663b1c8&page=${pageNo}&pageSize=${this.props.pageSize}`)
+
+   async componentDidMount(){
+    document.title = this.capitalize(this.props.category)+" | NewsApp"
+    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`)
     let newsArticles = await data.json()
     // This is the way we used always set the state of the state variables here in react.though we could have directly written
     // simply like other languages too
-    this.setState({articles: newsArticles.articles, totalArticles : newsArticles.totalResults, loading: false, page: pageNo})
+    this.setState({articles: newsArticles.articles, totalArticles : newsArticles.totalResults, loading: false})
+   
+}
 
-   }
-   async componentDidMount(){
-    document.title = this.capitalize(this.props.category)+" | NewsApp"
-    this.updateNews(1)
-   }
+   fetchMoreData = async (pageNo) => {
+    let data = await fetch(`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${pageNo}&pageSize=${this.props.pageSize}`)
+    let newsArticles = await data.json()
+    // This is the way we used always set the state of the state variables here in react.though we could have directly written
+    // simply like other languages too
+    
+    this.setState({articles: this.state.articles.concat(newsArticles.articles),totalArticles : newsArticles.totalResults, page: this.state.page +1})
+  };
 
-   handleNextPage = async ()=>{
-    this.updateNews(this.state.page +1)
-    }
-   handlePreviousPage = async()=>{
-   this.updateNews(this.state.page-1)
-   }
+  
     
   render() {
     return (
-        <div className='container'>
-            <h1 className='text-center' style={{margin: '40px'}}>NewsApp - Top {this.capitalize(this.props.category)} Headlines</h1>
-            <div className="row my-3 d-flex justify-content-center">
-                {this.state.loading && <Spinner/>}
+        <>
+            <h1 className='text-center my-5' style={{margin: '40px'}}>NewsApp - Top {this.capitalize(this.props.category)} Headlines</h1>
+            {this.state.loading && <Spinner/>}
+            <InfiniteScroll
+            // Here this dataLength is the length of the items that are currently being displayed and not the no of items that could
+            // be displayed instead.
+          dataLength={this.state.articles.length}
+          next={()=>this.fetchMoreData(this.state.page +1)}
+          // So the issue which was coming in this laoder was by-mistakenly i was calling totalResults which does not even exist
+          hasMore={this.state.articles.length !== this.state.totalArticles}
+          loader={<Spinner/>}
+        >
+          <div className="row my-3 d-flex justify-content-center">
                 {this.state.articles.map((element)=>{
                    return <div key={element.url} className="col-md-3 my-2 mx-3">
                     <NewsItem title={element.title?element.title.slice(0,40):""} description={element.description?element.description.slice(0,80):""} imgsource={element.urlToImage?element.urlToImage:"https://cdn.theathletic.com/app/uploads/2022/10/12091755/GettyImages-1240077873-1.jpg"} newsURL={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
@@ -59,11 +72,8 @@ export default class News extends Component {
                 })}   
                 
             </div>
-            <div className="container d-flex justify-content-between">
-            <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousPage}> &larr; Previous</button>
-            <button disabled={this.state.page +1 > Math.ceil(this.state.totalArticles/20)} type="button" className="btn btn-dark" onClick={this.handleNextPage}>Next &rarr;</button>
-            </div>
-        </div>
+        </InfiniteScroll>
+        </>
     )
   }
 }
